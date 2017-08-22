32f015c2c05791cde77d05507fe6972e67841e76
kabashima shiori <sor.0810@gmail.com>
2017-08-22 23:21:41 +0900

    eslintをいれたよ

--- tenki-bot.js
+++ tenki-bot.js
@@ -1,26 +1,26 @@
 const request = require('request');
 
 const headers = {
-    'Content-Type':'application/x-www-form-urlencoded'
-}//毎回変わる可能性があるから、外に出して定義する
+  'Content-Type': 'application/x-www-form-urlencoded',
+};// 毎回変わる可能性があるから、外に出して定義する
 
 const options = {
-    url: 'https://slack.com/api/chat.postMessage',
-    method: 'POST',
-    headers: headers,
-    json: true,
-    form: {
-        token:process.env.TOKEN,
-        channel:process.env.CHANNEL_ID,
-        text:'Hello,nishitani-shokudo.',
-    }
-}
-
-const handleResponse = function(error, response, body) {
-  console.log(error);
-  console.log('statusCode',response && response.statusCode);/*なんで「&&(←演算子)」なの？A.responseが帰ってこないこともあるから。response　がtrue相当だったら&&以降もみにいく
-  nullのプロパティを参照しようとするとerrorになるから*/
-  console.log(body);  
+  url: 'https://slack.com/api/chat.postMessage',
+  method: 'POST',
+  headers,
+  json: true,
+  form: {
+    token: process.env.TOKEN,
+    channel: process.env.CHANNEL_ID,
+    text: 'Hello,nishitani-shokudo.',
+  },
 };
 
-request(options,handleResponse);
+const handleResponse = (error, response, body) => {
+  console.log(error);
+  console.log('statusCode', response && response.statusCode);/* なんで「&&(←演算子)」なの？A.responseが帰ってこないこともあるから。responseがtrue相当だったら&&以降もみにいく
+  nullのプロパティを参照しようとするとerrorになるから */
+  console.log(body);
+};
+
+request(options, handleResponse);
