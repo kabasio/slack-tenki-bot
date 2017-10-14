e02db3e79c0d5d758a3be6e471f9240f796c8c95
kabashima shiori <sor.0810@gmail.com>
2017-10-14 14:48:14 +0900

    時間を8:30にしたよ

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const weather = require('./getweather');
 // 再帰関数(自分を呼び出す関数)
 const run = () => {
   const now = new Date();
-  if (now.getHours() === 23 && now.getMinutes() === 30) {
+  if (now.getHours() === 7 && now.getMinutes() === 30) {
     weather();
   }
-  setTimeout(run, 60000);
+  setTimeout(run, 10000);
 };
 
 run();
